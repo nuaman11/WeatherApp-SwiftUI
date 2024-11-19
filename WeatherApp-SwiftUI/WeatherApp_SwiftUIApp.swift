aaf4ec9628f7536c6698ef5cb63e4e4a23030f73
nuaman <nuaman.s@axeltechnologies.com>
2024-11-19 12:49:03 +0530

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Muhammed Nuaman Sadiq on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
